230df778b3fcc0cc8553b424ac589ef856d8b399
CHOIdotGit <tkdals9748@naver.com>
2024-09-27 18:12:53 +0900

    20240927_Share_your_travel_reviews!

---
+++ side_project/Share your travel reviews/src/index.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Share your travel reviews!</title>
+</head>
+<body>
+    <header class="header">
+            <a href="/">
+                <div class="main_title">
+                    <h1>Share your travel reviews!</h1>
+                </div>
+            </a>
+    </header>
+    <div class="container">
+        <main class="main-content">
+            <div class="card">
+                <div class="pop_card_img"></div>
+                <a href="/domestic.php"><h2 class="pop_card_title">인기글</h2></a>
+            </div>
+            <div class="card">
+                <div class="oversee_card_img"></div>
+                <a href="/oversee.php"><h2 class="oversee_card_title">해외 여행</h2></a>
+            </div>
+            <div class="card">
+                <div class="domestic_card_img"></div>
+                <a href="/pop.php"><h2 class="domestic_card_title">국내 여행</h2></a>
+            </div>
+        </main>
+    </div>    
+<footer class="footer">
+    © 2024. GREEN COMPUTER ART ACADEMY DAEGU / CREAT BY CHOI
+</footer>
+
+</body>
+</html>
---
+++ side_project/Share your travel reviews/src/oversee_reviews.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+<div>
+            <a href=""><button class="btn-small">이전</button></a>
+            <a href=""><button class="btn-small">1</button></a>
+            <a href=""><button class="btn-small">2</button></a>
+            <a href=""><button class="btn-small">3</button></a>
+            <a href=""><button class="btn-small">다음</button></a>
+        </div>
+</body>
+</html>
