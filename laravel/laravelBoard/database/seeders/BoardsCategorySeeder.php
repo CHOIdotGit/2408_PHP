0a6d148e2bf86c75aebc33a4b3d0e7514956f31b
CHOIdotGit <tkdals9748@naver.com>
2024-11-12 18:21:17 +0900

    24.11.12 오타수정

--- laravel/laravelBoard/database/seeders/BoardsCategorySeeder.php
+++ laravel/laravelBoard/database/seeders/BoardsCategorySeeder.php
@@ -18,8 +18,8 @@ class BoardsCategorySeeder extends Seeder
         // *시더는 롤백이 없음
         BoardsCategory::insert([
             ['bc_type' => '0', 'bc_name' => '자유게시판']
-            ,['bc_type' => '1', 'bc_name' => '자유게시판']
-            ,['bc_type' => '2', 'bc_name' => '질문게시판']
+            ,['bc_type' => '1', 'bc_name' => '질문게시판']
+            ,['bc_type' => '2', 'bc_name' => '문의게시판']
         ]); //php artisan db:seed --class=BoardsCategorySeeder
     }
 }
