12ad24c738cabf52d9005f7a8bc4d46335cbedb3
CHOIdotGit <tkdals9748@naver.com>
2024-11-18 10:49:58 +0900

    24.11.18 중복 파일 삭제

--- C/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Route\Route
-spl_autoload_register(function($path) {
-    require_once(str_replace('\\', '/', $path).'.php'); // Route\Route.php
-});
--- C/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// './' 생략가능(해당 파일과 같은 위치에 있을 경우)
-require_once('./config.php'); // 설정 파일 불러오기
-require_once('./autoload.php'); // 오토 로드 파일 불러오기
-
-new Route\Route(); // 라우터 호출
