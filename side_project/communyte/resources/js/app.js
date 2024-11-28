67a48a54236e1e6832ccaafce4e0caf068e994ea
CHOIdotGit <tkdals9748@naver.com>
2024-11-29 08:28:10 +0900

    laravel + vue communyty

---
+++ side_project/communyte/resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import AppComponent from '../views/components/AppComponent.vue';
+
+createApp({
+    components: {
+        AppComponent,
+    }
+})
+.mount('#app');
