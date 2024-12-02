69d999d4f587a595994c734e1a67e3a9b3cf4c60
CHOIdotGit <tkdals9748@naver.com>
2024-12-02 18:08:20 +0900

    까먹은 것

--- side_project/communyte/routes/api.php
+++ side_project/communyte/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/login', function(Request $request) {
+    // 이메일과 비밀번호 가져오기
+    $credentials = $request->only('email', 'password');
 
+    // 이메일을 기준으로 사용자 검색
+    $userInfo = User::where('email', $credentials['email'])->first();
+
+    if($userInfo && Hash::check($credentials['password'], $userInfo->password)) {
+        $token = $userInfo->createToken('YourAppName')->plainTextToken;
+    }
+});
